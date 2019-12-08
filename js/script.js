f019dfe02ac1b7a4c516d6c03ed42471497b1f4d
Kishore S Shenoy <kichappa@gmail.com>
2019-12-09 01:09:32 +0530

    refined preloader

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 // $('.splash').hide(0);
-$(document).ready(function () {
-});
 $(window).on('load', function(){
-    $('#preloader-image').delay(500).slideUp(350);
+    $('#preloader-image').delay(400).fadeOut(350);
     $('#preloader-text').delay(600).fadeOut(350);
     $('#preloader').delay(900).fadeOut();
-    $('.splash-text-hi').hide(0).delay(1300).fadeIn(1750).delay(500).fadeOut(1750);
-    $('.splash-text-name').hide(0).delay(1300).delay(4000).fadeIn(1750);
+    $('#splash-text-hi').hide(0).delay(1300).fadeIn(1750).delay(500).fadeOut(1750);
+    $('#splash-text-name').hide(0).delay(1300).delay(4000).fadeIn(1750);
 });
